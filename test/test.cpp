32c0aa339fd7b7f72639048f751eb4e4af8cc192
ntppro <mr.trongphuongpro@gmail.com>
2019-12-26 18:22:19 +0300

    Add crc32 checksum

--- test/test.cpp
+++ test/test.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <string.h>
 #include "uart.h"
+#include "crc32.h"
 
 using namespace std;
 using namespace BBB;
@@ -11,15 +12,22 @@ uint8_t preamble_1[4] = {0xAA, 0xBB, 0xCC, 0xDD};
 uint8_t preamble_2[4] = {0xAB, 0xBC, 0xCD, 0xDE};
 
 void sendMessage(UART& bus, const void* preamble, const void* data, uint32_t len) {
+	crc32_t checksum = crc32_compute(data, len);
+
 	bus.writeBuffer(preamble, 4);
 	bus.write(0xAA);
 	bus.write(0xBB);
 	bus.write(len);
 	bus.writeBuffer(data, len);
+	bus.writeBuffer(&checksum, 4);
+
 }
 
 
 int main(int argc, const char** argv) {
+
+	crc32_init();
+
 	UART bus(UART::UART1, 9600);
 
 	const char* s[4] = {"Hello, this's Beaglebone Black", "trongphuongpro",
